14decba29b5b052e15e4f2be4e54b488d119fc67
shunlir <shunlir>
2019-12-05 08:20:38 +0800

    emacs - add .doom.d

---
+++ emacs/.doom.d/config.el
@@ -0,0 +1,25 @@
+;;; .doom.d/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here
+
+(use-package! dap-mode
+  :after lsp-mode
+  :config
+  (dap-mode t)
+  (dap-ui-mode t))
+
+;; ccls implies use of lsp
+(after! ccls
+  (setq lsp-prefer-flymake nil)
+  (setq company-lsp-enable-recompletion t)
+  (setq-default flycheck-disabled-checkers '(c/c++-clang c/c++-cppcheck c/c++-gcc))
+  (setq ccls-sem-highlight-method 'font-lock)
+  (ccls-use-default-rainbow-sem-highlight)
+  (map!
+   :map (c-mode-map c++-mode-map)
+   :n "C-h" (λ! (ccls-navigate "U"))
+   :n "C-j" (λ! (ccls-navigate "R"))
+   :n "C-k" (λ! (ccls-navigate "L")))
+  )
+
+;;(setq ccls-args '("--log-file=/tmp/ccls.log"))
---
+++ emacs/.doom.d/packages.el
@@ -0,0 +1,10 @@
+;; -*- no-byte-compile: t; -*-
+;;; .doom.d/packages.el
+
+;;; Examples:
+;; (package! some-package)
+;; (package! another-package :recipe (:host github :repo "username/repo"))
+;; (package! builtin-package :disable t)
+
+(when (featurep! :lang cc +lsp)
+  (package! dap-mode))
