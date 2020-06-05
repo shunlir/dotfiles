e520f8b969a4c84ceeb7e770728b7642aba47b2c
shunlir <shunlir>
2020-06-05 21:16:55 +0800

    emacs - remove spacemacs

--- emacs/.emacs-profiles.el
+++ emacs/.emacs-profiles.el
@@ -1,3 +1,2 @@
 (("default" . ((user-emacs-directory . "~/.emacs.d-doom-emacs")))
- ("spacemacs" . ((user-emacs-directory . "~/.emacs.d-spacemacs")))
  ("doom" . ((user-emacs-directory . "~/.emacs.d-doom-emacs"))))
