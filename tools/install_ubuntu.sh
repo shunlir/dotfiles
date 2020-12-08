8cf18661f2432418609e23d5228460a31ba3ab8b
shunlir <shunlir>
2020-12-08 21:50:09 +0800

    tools - update ubuntu

--- tools/install_ubuntu.sh
+++ tools/install_ubuntu.sh
@@ -6,19 +6,35 @@ set -e
 # text mode
 #
 
+if [ "$(lsb_release -rs | cut -d. -f1)" = 18 ]; then
+  add-apt-repository -y ppa:neovim-ppa/stable
+  apt update -y
+fi
 apt install -y git vim wget lua50 zsh emacs tmux \
-    neovim python-neovim python3-neovim
+    neovim python-neovim python3-neovim golang
 
-wget -P /usr/local/bin/ https://raw.githubusercontent.com/so-fancy/diff-so-fancy/master/third_party/build_fatpack/diff-so-fancy
-chmod a+x /usr/local/bin/diff-so-fancy 
+if ! command -v diff-so-fancy >/dev/null 2>&1; then
+  wget -P /usr/local/bin/ https://raw.githubusercontent.com/so-fancy/diff-so-fancy/master/third_party/build_fatpack/diff-so-fancy
+  chmod a+x /usr/local/bin/diff-so-fancy
+fi
 
 [ -d /tmp/work] || mkdir -p /tmp/work
 
-wget -P /tmp/work https://github.com/sharkdp/fd/releases/download/v7.4.0/fd_7.4.0_amd64.deb
-dpkg -i /tmp/work/fd_7.4.0_amd64.deb
+if ! command -v fd >/dev/null 2>&1; then
+  wget -P /tmp/work https://github.com/sharkdp/fd/releases/download/v8.2.1/fd_8.2.1_amd64.deb
+  dpkg -i /tmp/work/fd_8.2.1_amd64.deb
+fi
 
-wget -P /tmp/work https://github.com/BurntSushi/ripgrep/releases/download/11.0.2/ripgrep_11.0.2_amd64.deb
-dpkg -i /tmp/work/ripgrep_11.0.2_amd64.deb
+if ! command -v rg >/dev/null 2>&1; then
+  wget -P /tmp/work https://github.com/BurntSushi/ripgrep/releases/download/12.1.1/ripgrep_12.1.1_amd64.deb
+  dpkg -i /tmp/work/ripgrep_12.1.1_amd64.deb
+fi
+
+if ! command -v fzf >/dev/null 2>&1; then
+  wget -P /tmp/work https://github.com/junegunn/fzf/releases/download/0.24.4/fzf-0.24.4-linux_amd64.tar.gz
+  tar xf /tmp/work/fzf-0.24.4-linux_amd64.tar.gz -C /usr/local/bin
+  chmod a+x /usr/local/bin/fzf
+fi
 
 rm -rf /tmp/work
 
