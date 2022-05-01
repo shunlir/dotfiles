814275cfa1639d08f4d367343903cfc9efb6e3dd
shunlir <shunlir>
2022-05-01 21:41:56 +0800

    nvim - nvim-cmp: fix vsnip tab

--- vim/.config/nvim/lua/cfg/cmp.lua
+++ vim/.config/nvim/lua/cfg/cmp.lua
@@ -2,6 +2,10 @@ return function()
   vim.o.pumheight = 20
   local cmp = require'cmp'
 
+  local feedkey = function(key, mode)
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
+  end
+
   cmp.setup {
     snippet = {
       expand = function(args)
@@ -14,20 +18,24 @@ return function()
       ['<C-/>'] = cmp.mapping.complete(),
       ['<C-e>'] = cmp.mapping.close(),
       ['<CR>'] = cmp.mapping.confirm({ select = true }),
-      ['<Tab>'] = function(fallback)
+      ['<Tab>'] = cmp.mapping(function(fallback)
         if cmp.visible() then
           cmp.select_next_item()
+        elseif vim.fn["vsnip#available"](1) == 1 then
+          feedkey("<Plug>(vsnip-expand-or-jump)", "")
         else
           fallback()
         end
-      end,
-      ['<S-Tab>'] = function(fallback)
+      end, { "i", "s" }),
+      ['<S-Tab>'] = cmp.mapping(function(fallback)
         if cmp.visible() then
           cmp.select_prev_item()
+        elseif vim.fn["vsnip#jumpable"](-1) == 1 then
+          feedkey("<Plug>(vsnip-jump-prev)", "")
         else
           fallback()
         end
-      end,
+      end, { "i", "s" }),
       ['<C-n>'] = function(fallback)
         if cmp.visible() then
           cmp.select_next_item()
