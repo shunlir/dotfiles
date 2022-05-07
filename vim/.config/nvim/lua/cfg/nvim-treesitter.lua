cbf475bc441d7bb580a4ac3e0531e416a6d0c047
shunlir <shunlir>
2022-05-07 20:14:47 +0800

    nvim - treesitter: fix break changes

--- vim/.config/nvim/lua/cfg/nvim-treesitter.lua
+++ vim/.config/nvim/lua/cfg/nvim-treesitter.lua
@@ -1,10 +1,11 @@
 return function()
-  require'nvim-treesitter.configs'.setup {
-    ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-    ignore_install = {}, -- List of parsers to ignore installing
+  require 'nvim-treesitter.configs'.setup {
+    ensure_installed = "all", -- A list of parser names, or "all"
+    sync_install = false, -- Install parsers synchronously (only applied to `ensure_installed`)
+    ignore_install = {}, -- List of parsers to ignore installing (for "all")
     highlight = {
-      enable = true,              -- false will disable the whole extension
-      disable = {},  -- list of language that will be disabled
+      enable = true, -- false will disable the whole extension
+      disable = {}, -- list of language that will be disabled
       -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
       -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
       -- Using this option may slow down your editor, and you may see some duplicate highlights.
