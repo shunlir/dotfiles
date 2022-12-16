13a5dd67edc8cc0ea16827d60159ec2bb4008c6f
shunlir <shunlir>
2022-12-16 21:28:49 +0800

    nvim - refine packer.nvim bootstrapping

--- vim/.config/nvim/lua/plugins.lua
+++ vim/.config/nvim/lua/plugins.lua
@@ -1,9 +1,16 @@
 -- bootstrap packer
-local fn = vim.fn
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-if fn.empty(fn.glob(install_path)) > 0 then
-  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
 end
+
+local packer_bootstrap = ensure_packer()
 
 return require('packer').startup(function(use)
   use {'wbthomason/packer.nvim'}
@@ -103,5 +110,9 @@ return require('packer').startup(function(use)
   use {'joshdick/onedark.vim', config = require('cfg.colors')}
 
   use {'tweekmonster/startuptime.vim', cmd = 'StartupTime'}
+
+  if packer_bootstrap then
+    require('packer').sync()
+  end
 
 end)
