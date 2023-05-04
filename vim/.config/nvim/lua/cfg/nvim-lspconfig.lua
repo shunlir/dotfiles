91ed47f04b41c0587119f14656e7fc9643eebe4a
shunlir <shunlir>
2023-05-04 23:26:08 +0800

    nvim - lua_ls: disable the message on every startup

--- vim/.config/nvim/lua/cfg/nvim-lspconfig.lua
+++ vim/.config/nvim/lua/cfg/nvim-lspconfig.lua
@@ -110,6 +110,8 @@ return function()
           workspace = {
             -- Make the server aware of Neovim runtime files
             library = vim.api.nvim_get_runtime_file("", true),
+            -- Disable the message on every startup 'Do you need to configure your work environment as xxx'
+            checkThirdParty = false,
           },
           -- Do not send telemetry data containing a randomized but unique identifier
           telemetry = {
